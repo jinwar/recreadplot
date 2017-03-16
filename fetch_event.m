13fca0f309a8ac0648e6e2770f8d3132ee48b8a1
Zach Eilon <zeilon@ldeo.columbia.edu>
2017-03-16 13:11:37 -0400

    modifying data download script to allow bulk downloads with BREQFAST

--- fetch_event.m
+++ fetch_event.m
@@ -24,13 +24,13 @@ end
 for ie = 1:length(events_info)
 	plotm(evlas(ie),evlos(ie),'ko','markersize',round(mags(ie)*4));
 end
-[temp first_eventid] = min(bg_times);
-[temp biggest_eventid] = max(mags);
+[~,first_eventid] = min(bg_times);
+[~,biggest_eventid] = max(mags);
 plotm(evlas(first_eventid),evlos(first_eventid),'bo','markersize',round(mags(first_eventid)*4));
 plotm(evlas(biggest_eventid),evlos(biggest_eventid),'ro','markersize',round(mags(biggest_eventid)*4));
-[mlat mlon] = inputm(1);
+[mlat,mlon] = inputm(1);
 dists = distance(mlat,mlon,evlas,evlos);
-[temp eventid] = min(dists);
+[~,eventid] = min(dists);
 plotm(evlas(eventid),evlos(eventid),'ko','markersize',round(mags(eventid)*4),'markerfacecolor','g');
 
 disp(['Event: ',events_info(eventid).PreferredTime,' Mag: ', num2str(mags(eventid))]);
--- setup_parameters.m
+++ setup_parameters.m
@@ -19,7 +19,7 @@ min_epi_dist = 0;
 max_epi_dist = 100;
 
 % define donwload waveform length
-align_phase = 'P';   % O for original time, P for P phase
+align_phase = 'P';   % 'O' for original time, 'P' for P phase (can actually use any phase, but not recommended other than O|P|S)
 min_before = 10;   % minutes before the phase
 min_after = 60; %  minutes after the phase
 
